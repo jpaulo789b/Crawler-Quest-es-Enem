681f4193479d86c4b7b463005c892f6079f36c51
João Paulo Santos Almeida <789joaopaulo@gmail.com>
2017-06-17 20:25:27 -0300

    Ok

--- indo/spiders/spiderquestoes.py
+++ indo/spiders/spiderquestoes.py
@@ -1,13 +1,36 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.spiders import Rule
+from scrapy.linkextractors import LinkExtractor
 from lxml import html
-import re
 from indo.items import IndoItem
+
+
 class SpiderquestoesSpider(scrapy.Spider):
     name = "spiderquestoes"
     allowed_domains = ["www.gabarite.com.br"]
-    start_urls = ['http://www.gabarite.com.br/questoes-de-concursos/disciplina/1-portugues']
+    start_urls = [
+            'http://www.gabarite.com.br/questoes-de-concursos/disciplina/13-administracao',
+            'http://www.gabarite.com.br/questoes-de-concursos/disciplina/85-administracao-financeira-e-orcamentaria',
+            'http://www.gabarite.com.br/questoes-de-concursos/disciplina/68-agronomia',
+            'http://www.gabarite.com.br/questoes-de-concursos/disciplina/12-arquivologia',
+            'http://www.gabarite.com.br/questoes-de-concursos/disciplina/75-atualidades',
+            'http://www.gabarite.com.br/questoes-de-concursos/disciplina/69-biblioteconomia',
+            'http://www.gabarite.com.br/questoes-de-concursos/disciplina/20-direito-do-trabalho',
+            'http://www.gabarite.com.br/questoes-de-concursos/disciplina/39-geografia',
+            'http://www.gabarite.com.br/questoes-de-concursos/disciplina/38-historia',
+            'http://www.gabarite.com.br/questoes-de-concursos/disciplina/37-ingles',
+            'http://www.gabarite.com.br/questoes-de-concursos/disciplina/1-portugues',
+            'http://www.gabarite.com.br/questoes-de-concursos/disciplina/40-quimica',
+            'http://www.gabarite.com.br/questoes-de-concursos/disciplina/78-telecomunicacoes',
+            'http://www.gabarite.com.br/questoes-de-concursos/disciplina/4-informatica',
+            'http://www.gabarite.com.br/questoes-de-concursos/disciplina/89-governanca-de-ti',
+            'http://www.gabarite.com.br/questoes-de-concursos/disciplina/61-direitos-humanos',
+            'http://www.gabarite.com.br/questoes-de-concursos/disciplina/51-biologia'
+    ]
+
     def parse(self, response):
+
         tree = html.parse(response.url)
         conteudo = tree.xpath('//*[@id="content_questoes_conteudo"]')[0]
         forms = conteudo.xpath('form')
@@ -28,10 +51,28 @@ class SpiderquestoesSpider(scrapy.Spider):
             item['alternativa_C'] = alternativas[2].text if len(alternativas) > 2 else ''
             item['alternativa_D'] = alternativas[3].text if len(alternativas) > 3 else ''
             item['alternativa_E'] = alternativas[4].text if len(alternativas) > 4 else ''
-            item['alternativaCorreta'] = forms[idx].xpath('button/@onclick')[0];
+            item['alternativaCorreta'] = forms[idx].xpath('button/@onclick')[0]
             inputString = str(item['alternativaCorreta']).split("checaquestao", 1)[1]
             item['alternativaCorreta'] = str(inputString).split(',')[1]
             yield item
             print item
-            for next_page in response.css('div#content_paginacao > a'):
-                yield response.follow(next_page, self.parse)
+        for link in response.css('#content_paginacao > a::attr(href)').extract():
+            yield scrapy.Request(response.urljoin(link), callback=self.parse)
+
+#     'http://www.gabarite.com.br/questoes-de-concursos/disciplina/13-administracao',
+#     'http://www.gabarite.com.br/questoes-de-concursos/disciplina/85-administracao-financeira-e-orcamentaria',
+#     'http://www.gabarite.com.br/questoes-de-concursos/disciplina/68-agronomia',
+#     'http://www.gabarite.com.br/questoes-de-concursos/disciplina/12-arquivologia',
+#     'http://www.gabarite.com.br/questoes-de-concursos/disciplina/75-atualidades',
+#     'http://www.gabarite.com.br/questoes-de-concursos/disciplina/69-biblioteconomia',
+#     'http://www.gabarite.com.br/questoes-de-concursos/disciplina/20-direito-do-trabalho',
+#     'http://www.gabarite.com.br/questoes-de-concursos/disciplina/39-geografia',
+#     'http://www.gabarite.com.br/questoes-de-concursos/disciplina/38-historia',
+#     'http://www.gabarite.com.br/questoes-de-concursos/disciplina/37-ingles',
+#     'http://www.gabarite.com.br/questoes-de-concursos/disciplina/1-portugues',
+#     'http://www.gabarite.com.br/questoes-de-concursos/disciplina/40-quimica',
+#     'http://www.gabarite.com.br/questoes-de-concursos/disciplina/78-telecomunicacoes',
+#     'http://www.gabarite.com.br/questoes-de-concursos/disciplina/4-informatica',
+#     'http://www.gabarite.com.br/questoes-de-concursos/disciplina/89-governanca-de-ti',
+#     'http://www.gabarite.com.br/questoes-de-concursos/disciplina/61-direitos-humanos',
+#     'http://www.gabarite.com.br/questoes-de-concursos/disciplina/51-biologia'
